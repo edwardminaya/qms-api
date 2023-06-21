8790562356a204d9cbbc317589e0c06d3c6c8548
“Edward <eminaya49@gmail.com>
2023-06-21 17:23:46 -0500

    created update and delete actions. tested with http request

--- app/controllers/productclasses_controller.rb
+++ app/controllers/productclasses_controller.rb
@@ -11,11 +11,28 @@ class ProductclassesController < ApplicationController
   end
 
   def create
+    # create happy sad path later
     @productclass = Productclass.new(
-      product_class_name: params[:product_class_name] || productclass.product_class_name,
-      product_class_code: params[:product_class_code] || productclass.product_class_code,
+      product_class_name: params[:product_class_name],
+      product_class_code: params[:product_class_code],
     )
     @productclass.save
     render :show
+  end
+
+  def update
+    # create happy sad path later
+    @productclass = Productclass.find_by(id: params[:id])
+    @productclass.update(
+      product_class_name: params[:product_class_name] || @productclass.product_class_name,
+      product_class_code: params[:product_class_code] || @productclass.product_class_code,
+    )
+    render :show
+  end
+
+  def destroy
+    @productclass = Productclass.find_by(id: params[:id])
+    @productclass.destroy
+    render json: { message: "Product class removed from list" }
   end
 end
