1235a429e5867afba2a8e3eef8727f1cf2f66970
“Edward <eminaya49@gmail.com>
2023-06-22 16:07:21 -0500

    create action made and tested with http reqeust

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  # Only admin can create products
   def index
     @products = Product.all
     render :index
@@ -6,6 +7,17 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find_by(id: params[:id])
+    render :show
+  end
+
+  def create
+    @product = Product.new(
+      product_name: params[:product_name],
+      product_code: params[:product_code],
+      product_description: params[:product_description],
+      productclass_id: params[:productclass_id],
+    )
+    @product.save
     render :show
   end
 end
